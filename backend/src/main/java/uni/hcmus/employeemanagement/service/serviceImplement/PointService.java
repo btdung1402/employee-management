010833a1fe3a379d6c9d015f2699d795907e96b4
btdung1402 <dungb.work1402@gmail.com>
2024-11-16 13:10:52 +0000

    add API search

--- backend/src/main/java/uni/hcmus/employeemanagement/service/serviceImplement/PointService.java
+++ backend/src/main/java/uni/hcmus/employeemanagement/service/serviceImplement/PointService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uni.hcmus.employeemanagement.dto.EmployeeDto;
 import uni.hcmus.employeemanagement.dto.EmployeePointDto;
 import uni.hcmus.employeemanagement.entity.Employee;
 import uni.hcmus.employeemanagement.entity.Manager;
