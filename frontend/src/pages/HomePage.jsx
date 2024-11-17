b0859e14131418f76b1ca3c73c92bb276e936ebe
btdung1402 <dungb.work1402@gmail.com>
2024-11-17 07:48:53 +0000

    Edit HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -23,25 +23,10 @@ const HomePage = () => {
     };
 
     return (
-        /*<div className="container mt-5">
-            <div>123444</div>
-            <h1 className="text-center mb-4">Home Page</h1>
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="input-group mb-3">
-                        <input
-                            type="text"
-                            className="form-control"
-                            value={employeeId}
-                            onChange={(e) => setEmployeeId(e.target.value)}
-                            placeholder="Enter Employee ID"
-                        />
-                        <button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
-                    </div>
-                </div>
-            </div>
+        <div className="container mt-5">
+            
         </div>
-    );*/
+    );
 };
 
 export default HomePage;
